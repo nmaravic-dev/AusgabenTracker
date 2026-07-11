96c7b9ad6b2ebb73c87e63ca01598596b9957ba4
Barulica <barulabarula@gmail.com>
2026-07-11 14:03:56 +0200

    Add MainViewModel with properties, commands and data loading

--- AusgabenTracker/ViewModels/ViewModelBase.cs
+++ AusgabenTracker/ViewModels/ViewModelBase.cs
@@ -12,5 +12,13 @@ namespace AusgabenTracker.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
